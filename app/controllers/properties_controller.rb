26ad0f110200a9b555af5af9403cefff87c9337c
miu oohira <miu.000408@gmail.com>
2021-03-09 16:31:40 +0900

    モデルの関連づけ

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,34 +1,29 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: [:show, :edit, :update, :destroy]
 
-  # GET /properties
-  # GET /properties.json
   def index
     @properties = Property.all
   end
 
-  # GET /properties/1
-  # GET /properties/1.json
   def show
+    @nearest_stations = @property.nearest_stations
   end
 
-  # GET /properties/new
   def new
     @property = Property.new
+    2.times { @property.nearest_stations.build }
   end
 
-  # GET /properties/1/edit
   def edit
+    @property.nearest_stations.build
   end
 
-  # POST /properties
-  # POST /properties.json
   def create
     @property = Property.new(property_params)
 
     respond_to do |format|
       if @property.save
-        format.html { redirect_to @property, notice: 'Property was successfully created.' }
+        format.html { redirect_to @property, notice: '物件情報を登録しました' }
         format.json { render :show, status: :created, location: @property }
       else
         format.html { render :new }
@@ -37,12 +32,10 @@ class PropertiesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /properties/1
-  # PATCH/PUT /properties/1.json
   def update
     respond_to do |format|
       if @property.update(property_params)
-        format.html { redirect_to @property, notice: 'Property was successfully updated.' }
+        format.html { redirect_to @property, notice: '物件情報を編集しました' }
         format.json { render :show, status: :ok, location: @property }
       else
         format.html { render :edit }
@@ -51,24 +44,34 @@ class PropertiesController < ApplicationController
     end
   end
 
-  # DELETE /properties/1
-  # DELETE /properties/1.json
   def destroy
     @property.destroy
     respond_to do |format|
-      format.html { redirect_to properties_url, notice: 'Property was successfully destroyed.' }
+      format.html { redirect_to properties_url, notice: '物件情報を削除しました' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_property
       @property = Property.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def property_params
-      params.require(:property).permit(:property_name, :rent, :address, :age, :note)
+      params.require(:property).permit(
+        :property_name, 
+        :rent, 
+        :address, 
+        :age, 
+        :note,
+        nearest_stations_attributes:[
+        :route_name,
+        :station_name,
+        :time,
+        :property_id,
+        :id,
+        :_destroy,
+        ],
+      )
     end
 end
