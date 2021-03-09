5f8aa01d07e911ac72603cc484e3ab09940d5c2f
miu oohira <miu.000408@gmail.com>
2021-03-09 13:43:54 +0900

    物件テーブル作成

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,10 @@ Bundler.require(*Rails.groups)
 
 module DevExam
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      # この二行の記述で自動生成しない設定
+      g.assets false
+      g.helper false
+    end
   end
 end
